1a0577128ede3df15ad4ce83b76f386685fd647a
Rekkyn <reedesler@gmail.com>
2013-07-27 20:04:01 -0700

    Added Game class

---
+++ src/rekkyn/environment/Game.java
@@ -0,0 +1,33 @@
+package rekkyn.environment;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game extends StateBasedGame {
+    
+    static AppGameContainer appgc;
+    public static final String NAME = "Environment";
+    public static final int MENU = 0;
+    public static final int PLAY = 1;
+    public static int width = 800;
+    public static int height = 600;
+
+    public Game(String name) {
+        super(name);
+    }
+    
+    public static void main(String[] args) {
+        try {
+            appgc = new AppGameContainer(new Game(NAME));
+            appgc.setDisplayMode(width, height, false);
+            appgc.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initStatesList(GameContainer container) throws SlickException {}
+}
