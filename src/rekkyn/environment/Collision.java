35d3827ce0b35e012bf94a0bd42ce5c4a2d0dd05
Rekkyn <reedesler@gmail.com>
2013-07-29 23:40:29 -0700

    Made physics actually work!

--- src/rekkyn/environment/Collision.java
+++ src/rekkyn/environment/Collision.java
@@ -3,10 +3,11 @@ package rekkyn.environment;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Collision {
-
+    
     public static void doCollision(Entity A, Entity B) {
-     // Calculate relative velocity
-        Vector2f rv = B.velocity.sub(A.velocity);
+        // Calculate relative velocity
+        Vector2f Bvelocity = B.velocity.copy();
+        Vector2f rv = Bvelocity.sub(A.velocity);
         
         // Calculate overlap on x axis
         float xOverlap = A.edgeLength / 2 + B.edgeLength / 2 - Math.abs(B.x - A.x);
@@ -18,20 +19,20 @@ public class Collision {
             
             // Calculate overlap on y axis
             float yOverlap = A.edgeLength / 2 + B.edgeLength / 2 - Math.abs(B.y - A.y);
-                        
+            
             // SAT test on y axis
             if (yOverlap > 0) {
                 // Find out which axis is axis of least penetration
                 if (xOverlap < yOverlap) {
                     // Point towards B knowing that n points from A to B
-                    if (B.x - A.x > 0) {
+                    if (B.x - A.x < 0) {
                         normal.set(-1, 0);
                     } else {
                         normal.set(1, 0);
                     }
                 } else {
                     // Point toward B knowing that n points from A to B
-                    if (B.y - A.y > 0) {
+                    if (B.y - A.y < 0) {
                         normal.set(0, -1);
                     } else {
                         normal.set(0, 1);
@@ -40,11 +41,9 @@ public class Collision {
             }
         }
         
-        
         // Calculate relative velocity in terms of the normal direction
         float velAlongNormal = rv.dot(normal);
         
-
         // Do not resolve if velocities are separating
         if (velAlongNormal > 0) {
             return;
@@ -55,19 +54,15 @@ public class Collision {
         
         // Calculate impulse scalar
         float j = -(1F + restitution) * velAlongNormal;
-        
-        
         j /= 1F / A.size + 1F / B.size;
-                
+        
         // Apply impulse
         Vector2f impulse = new Vector2f(normal.scale(j));
-
         
-        A.velocity.sub(impulse.scale(1 / A.size));
-        B.velocity.add(impulse.scale(1 / B.size));
-        System.out.println(A.velocity.toString());
-
+        Vector2f impulse2 = impulse.copy();
         
+        A.velocity.sub(impulse.scale(1F / A.size));
+        B.velocity.add(impulse2.scale(1F / B.size)); //TODO: make forces jazz
     }
     
 }
